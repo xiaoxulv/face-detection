fde60035879741b7889f0f45c5482ac0ba864fde
Xiaoxu <xiaoxul@andrew.cmu.edu>
2015-03-27 16:35:33 -0400

    bad performance, still finding bugs

--- problem2/adaBoost.m
+++ problem2/adaBoost.m
@@ -1,77 +1,78 @@
+function [E,mineigen,minthresh,alpha]= adaBoost()
+    facelocation = 'BoostingData/train/face/';
+    [F,facesize] = face(facelocation);
+    nonfacelocation = 'BoostingData/train/non-face/';
+    [NF,~] = face(nonfacelocation);
 
-facelocation = 'BoostingData/train/face/';
-[F,facesize] = face(facelocation);
-nonfacelocation = 'BoostingData/train/non-face/';
-[NF,nfsize] = face(nonfacelocation);
+    location = 'lfw1000/';
+    E = eigens(location,facesize);
+    % weight of eigenfaces in face and non-face
+    Wf = pinv(E)*F;
+    Wnf = pinv(E)*NF;
+    W = [Wf Wnf];
 
-location = 'lfw1000/';
-E = eigens(location,facesize);
-% weight of eigenfaces in face and non-face
-Wf = pinv(E)*F;
-Wnf = pinv(E)*NF;
-W = [Wf Wnf];
-
-fnum = size(Wf,2);
-nnum = size(Wnf,2);
-total = size(W,2);
-wunit = 1/total;
-weight = repmat(wunit,total,1);% initial weight for all
-ff = ones(fnum,1);
-nn = zeros(nnum,1);
-real = [ff;nn];% real classification for all
+    fnum = size(Wf,2);
+    nnum = size(Wnf,2);
+    total = size(W,2);
+    wunit = 1/total;
+    weight = repmat(wunit,total,1);% initial weight for all
+    ff = ones(fnum,1);
+    nn = zeros(nnum,1);
+    real = [ff;nn];% real classification for all
 
 
-% adaboost loop, rounds
-round = 5;
-alpha = zeros(round,1);
-mineigen = zeros(round,1);
-minthresh = 100;
-minthresh = repmat(minthresh,round,1);
+    % adaboost loop, rounds
+    round = 5;
+    alpha = zeros(round,1);
+    mineigen = zeros(round,1);
+    minthresh = 100;
+    minthresh = repmat(minthresh,round,1);
 
-for r = 1:round
-    minerr = 100;
-    minapply = zeros(total,1);
-    for i = 1:10 % for 10 eigenfaces
-        maxV = max(W(i,:));
-        minV = min(W(i,:));
-        unit1 = (maxV-minV)/100;
-        for k = 1:99 % for 100 gaps
-            thresh = minV + k*unit1;
-            % predict classification for all
-            apply = zeros(total,1);
-            err = 0;
-            for j = 1:total %for all the training data
-                if W(i,j) > thresh
-                    apply(j) = 1;
+    for r = 1:round
+        minerr = 100;
+        minapply = zeros(total,1);
+        for i = 1:10 % for 10 eigenfaces
+            maxV = max(W(i,:));
+            minV = min(W(i,:));
+            unit1 = (maxV-minV)/100;
+            for k = 1:99 % for 100 gaps
+                thresh = minV + k*unit1;
+                % predict classification for all
+                apply = zeros(total,1);
+                err = 0;
+                for j = 1:total %for all the training data
+                    if W(i,j) > thresh
+                        apply(j) = 1;
+                    end
+                    % error weight sum
+                    if apply(j) ~= real(j)
+                        err = err + weight(j);
+                    end
                 end
-                % error weight sum
-                if apply(j) ~= real(j)
-                    err = err + weight(j);
-                end
+                % update minimum error
+                if(err < minerr)
+                    minerr = err;
+                    minthresh(r) = thresh;
+                    mineigen(r) = i;
+                    minapply = apply;
+                end 
             end
-            % update minimum error
-            if(err < minerr)
-                minerr = err;
-                minthresh(r) = thresh;
-                mineigen(r) = i;
-                minapply = apply;
-            end 
         end
-    end
-    % calculate alpha
-    alpha(r) = 0.5*log((1-minerr)/minerr);
-    % new weight
-    for j = 1:total
-        if minapply(j) == real(j)
-            weight(j) = weight(j) * exp(-alpha(r));
-        else
-            weight(j) = weight(j)* exp(alpha(r));
+        % calculate alpha
+        alpha(r) = 0.5*log((1-minerr)/minerr);
+        % new weight
+        for j = 1:total
+            if minapply(j) == real(j)
+                weight(j) = weight(j) * exp(-alpha(r));
+            else
+                weight(j) = weight(j)* exp(alpha(r));
+            end
         end
-    end
-    % noramlize new weight
-    sumw = sum(weight);
-    for j = 1:total
-        weight(j) = weight(j)/sumw;
+        % noramlize new weight
+        sumw = sum(weight);
+        for j = 1:total
+            weight(j) = weight(j)/sumw;
+        end
     end
 end
 
--- problem2/test.m
+++ problem2/test.m
@@ -1,2 +1,52 @@
-location = 'lfw1000/';
-E = eigens(location,19);
+% location = 'lfw1000/';
+% E = eigens(location,19);
+
+% train to get strong classifier from weak classifiers
+[E,mineigen,minthresh,alpha] = adaBoost();
+
+[testface, ~] = face('BoostingData/test/face/');
+[testnonface,~] = face('BoostingData/test/non-face/');
+Wf = pinv(E)*testface;
+Wnf = pinv(E)*testnonface;
+W = [Wf Wnf];
+num = size(W,2);
+val = zeros(num,1);
+res = zeros(num,1);
+
+real = [ones(size(Wf,2),1);zeros(size(Wnf,2),1)];
+
+for i = 1:size(mineigen,1)
+    e = mineigen(i);
+    a = alpha(i); 
+    for j = 1:num
+        if W(i,j) > minthresh(i)
+            val(j) = val(j) + a;
+        else
+            val(j) = val(j) - a;
+        end
+    end
+end
+
+for i = 1:num
+    if res(i) > 0
+        res(i) = 1;
+    else
+        res(i) = 0;
+    end
+end
+count1 = 0;
+count2 = 0;
+for i = 1:size(Wf,2)
+    if res(i) == real(i)
+        count1 = count1 + 1;
+    end
+end
+rate1 = count1/size(Wf,2);
+for i = size(Wf,2)+1:size(Wnf,2)
+    if res(i) == real(i)
+        count2 = count2 + 1;
+    end
+end
+rate2 = count2/size(Wnf,2);
+beep;
+        
